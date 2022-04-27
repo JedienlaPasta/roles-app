29c2f914a2de1fe84770f42437d04bcf004e2690
seba_algarrobo <sebastian1377@gmail.com>
2022-04-27 10:22:27 -0400

    few changes for uploading to heroku

--- server/routes/roles.js
+++ server/routes/roles.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getRoles, createRol, updateRol, deleteRol, getRolesByRUT, getRolesByDIR } from '../controllers/roles.js'
+import { getRoles, getRolesByRUT, getRolesByDIR } from '../controllers/roles.js'
 import { authenticateToken } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
@@ -7,8 +7,8 @@ const router = express.Router()
 router.get('/rol', authenticateToken, getRoles)
 router.get('/rut', getRolesByRUT)
 router.get('/dir', getRolesByDIR)
-router.post('/', createRol)
-router.patch('/:id', updateRol)
-router.delete('/:id', deleteRol)
+// router.post('/', createRol)
+// router.patch('/:id', updateRol)
+// router.delete('/:id', deleteRol)
 
 export default router
